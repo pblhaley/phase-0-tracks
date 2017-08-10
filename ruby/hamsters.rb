735c68456e859c442c84959eb58ad7c223950923
Peter Haley <pblhaley@gmail.com>
2017-08-09 19:56:55 -0500

    Update data types

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,21 +1,25 @@
 puts "What is the hampster's name?"
 name=gets.chomp
+p name
 
 puts "How loud is the hampster? Please choose a level from 1-10, 10 being the loudest."
 volume=gets.chomp
+p volume.to_i
 
 puts "What is the hampster's fur color?"
 fur=gets.chomp
+p fur
 
 puts "What is the hampster's estimated age?"
-age=gets.chomp
+age=gets.strip
 
-if age == " "
-  age = nil
+if age.empty?
+  age=nil
 else
-  age=gets.chomp
+  age=age.to_f
 end
+p age
 
 puts "Is this hampster a good candidate for adoption? y/n"
 adoption=gets.chomp
-
+p adoption
