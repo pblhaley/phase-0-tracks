e743c3647087e3dd5fcec907713af5f32b6bafac
Peter Haley <pblhaley@gmail.com>
2017-08-14 09:46:35 -0500

    Initial commit

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+puts "What is your name?"
+name=gets.chomp
+
+puts "What is your age?"
+age=gets.chomp
+
+puts "What year were you born?"
+birth_year=gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you? y/n"
+garlic=gets.chomp
+
+puts "Would you like to enroll in the company's health insurance? y/n"
+insurance=gets.chomp
