91c2360d2e9b43387857fa54a37f3724b3efdce2
Peter Haley <pblhaley@gmail.com>
2017-08-21 17:08:51 -0500

    Create an array and a hash to call .each and .map! on

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,6 +1,50 @@
-def lunch
-  puts "What's for lunch?"
-  yield("soup", "salad", "sweet tea")
+#RELEASE 0
+#def lunch
+#  puts "What's for lunch?"
+#  yield("soup", "salad", "sweet tea")
+#end
+
+#lunch{|item1, item2, item3| puts "We're having #{item1} and #{item2} with #{item3} to drink!"}
+
+
+
+#RELEASE 1
+colors = ["red", "orange", "yellow", "blue", "purple"]
+
+puts "Original data:"
+p colors
+
+colors.each do |color|
+  puts color
 end
 
-lunch{|item1, item2, item3| puts "We're having #{item1} and #{item2} with #{item3} to drink!"}
+puts "After .each call:"
+p colors
+
+modified_colors = colors.map! do |color|
+  puts color
+  color.upcase
+end
+
+puts "After .map! call:"
+p colors
+
+####################
+
+books = {
+  "Twilight" => "Stephanie Meyer",
+  "Harry Potter" => "JK Rowling",
+  "The Dictionary" => "Merriam-Webster",
+  "To Kill a Mockingbird" => "Harper Lee",
+  "Carrie" => "Stephen King"
+}
+
+puts "Original data:"
+p books
+
+books.each do |title, author|
+ puts "#{title} was written by #{author}."
+end
+
+puts "After .each call:"
+p books
