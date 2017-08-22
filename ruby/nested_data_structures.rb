86b99a49584afe26023f92d9de0492fd77e45d0c
Peter Haley <pblhaley@gmail.com>
2017-08-21 19:19:02 -0500

    Create nested data structure for a zoo

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,78 @@
+zoo = {
+    aquarium: {
+        name: 'Aquarium',
+        areas: {
+            fresh_water: {
+                fresh_water_animals: [
+                    "snapping turtle",
+                    "goldfish",
+                    "salamander"
+                ]
+            },
+            salt_water: {
+                salt_water_animals: [
+                    "octopus",
+                    "sting ray",
+                    "sea turtle"
+                ]
+            },
+        },
+    },
+    desert: {
+        name: 'Desert Animals',
+        areas: {
+            mammals: {
+                desert_mammals: [
+                    "coyote",
+                    "armadillo",
+                    "dingo"
+                ]
+            },
+            reptiles: {
+                desert_reptiles: [
+                    "bearded lizard",
+                    "rattlesnake",
+                    "desert iguana"
+                ]
+            },
+        },
+    },
+    jungle: {
+        name: 'Jungle Animals',
+        areas: {
+            birds: {
+                jungle_birds: [
+                    "toucan",
+                    "parrot",
+                    "raptor"
+                ]
+            },
+            mammals: {
+                jungle_mammals: [
+                    "panther",
+                    "gorilla",
+                    "tiger"
+                ]
+            },
+        },
+    },
+    mountain: {
+        name: 'Mountain Animals',
+        areas: {
+            birds: {
+                mountain_birds: [
+                    "cardinal",
+                    "eagle",
+                    "hawk"
+                ]
+            },
+            mammals: {
+                mountain_mammals: [
+                    "bobcat",
+                    "grizzly bear",
+                    "raccoon"
+                ]
+            },
+        },
+    },
+}
