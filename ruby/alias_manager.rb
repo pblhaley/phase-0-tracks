f25557dac92147848c9fa31b6fe4a0b9fd2de6e5
Peter Haley <pblhaley@gmail.com>
2017-08-22 20:59:01 -0500

    Rewrite vowel and consonant code so that it is all in one place

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -6,24 +6,40 @@
 # Convert the new string into an array of all it's characters.
 # Iterate through the new array and if a character is a vowel, switch it to the next vowel in the sequence: aeiou.
 # Iterate through the new array and if the character is a consonate, switch it to the next consonant in the alphabet (be sure to skip over vowels, ie. d would become f).
-# Put this second new array back into string form.
+# Put this second new array back into string form and capitalize appropriately.
 # Print the string.
 
-def alias_manager(original_name)
-  reverse_words = original_name.to_s.downcase.split(' ').reverse!.join(' ')
-  characters = reverse_words.chars
-  characters.length.times do |i|
-    vowels = ["a", "e", "i", "o", "u"]
-    consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
-      if vowels.include?(characters[i])
-        characters[i] == vowels[vowels.index(characters[i]).next]
-      end
-      i += 1
+loop do
+  puts "Please enter the first and last name of the person who needs an alias. Please enter 'quit' when you are ready to exit."
+  original_name = gets.chomp
+  break if original_name.downcase == "quit"
+
+  reverse_name = original_name.to_s.downcase.split(' ').reverse!.join(' ')
+  #p reverse_name
+
+  characters = reverse_name.chars
+  #p characters
+
+  vowels = ["a", "e", "i", "o", "u"]
+  consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
+  i = 0
+  until i == characters.length
+    if characters[i] == "u"
+      characters[i] = vowels[0]
+    elsif characters[i] == "z"
+      characters[i] = consonants[0]
+    elsif vowels.include?(characters[i])
+      characters[i] = vowels[vowels.index(characters[i]).next]
+    elsif consonants.include?(characters[i])
+      characters[i] = consonants[consonants.index(characters[i]).next]
+    end
+    i += 1
   end
+#  p characters
+
   alias_name = characters.join.split(' ')
-  alias_name.last.capitalize!
-  alias_name.first.capitalize!
-  alias_name.join(' ')
+  cap_alias = alias_name.map{|i| i.capitalize}.join(' ')
+  puts cap_alias
+
 end
 
-p alias_manager("Peter Haley")
