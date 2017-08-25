589c8622a0e41b2367710208503c32852a0e838c
Peter Haley <pblhaley@gmail.com>
2017-08-25 17:40:03 -0500

    Write methods to add an item, remove an item, update a quanitity, and print the final list.

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -7,20 +7,55 @@
   # print the list to the console [can you use one of your other methods here? Maybe the one below]
 # output: hash that includes all items as keys and quantities as values
 
+
+def create_grocery_list(items)
+  split_items = items.split(' ')
+  list = {}
+  split_items.each {|item| list[item] = 1}
+  list
+end
+p items = "apples oranges bananas"
+p list = create_grocery_list(items)
+
+
 # Method to add an item to a list
 # input: list, item name, and optional quantity
 # steps: create a quantity value associated with an item key in the list hash
 # output: updated list hash
+
+
+def add_item(list, item, quantity)
+  list[item] = quantity
+  list
+end
+p add_item(list, "ice cream", 3)
+
 
 # Method to remove an item from the list
 # input: list hash, item name
 # steps: remove item from list hash
 # output: updated list hash
 
+
+def delete_item(list, item)
+  list.delete(item)
+  list
+end
+p delete_item(list, "apples")
+
+
 # Method to update the quantity of an item
 # input: list, item name, updated quantity
 # steps: set item name equal to a new quantity permanently
 # output: updated list hash
+
+
+def update_quantity(list, item, updated_quantity)
+  list[item] = updated_quantity
+  list
+end
+p update_quantity(list, "oranges", 5)
+
 
 # Method to print a list and make it look pretty
 # input: list
@@ -29,12 +64,11 @@
   # print each item and quanitity for all items on the list
 # output: list of all items and quantities
 
-def create_grocery_list(items)
-  split_items = items.split(' ')
-  list = {}
-  split_items.each {|item| list[item] = 1}
-  list
-end
 
-p items = "apples oranges bananas"
-p list = create_grocery_list(items)
+def print_list(list)
+  puts "This week's grocery list:"
+  list.each do |item, quantity|
+    puts "#{item}: #{quantity}"
+  end
+end
+print_list(list)
