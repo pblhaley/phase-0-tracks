514cdc25ee4a98290ab1513fac11eeb330adda49
Peter Haley <pblhaley@gmail.com>
2017-08-26 12:42:28 -0500

    Create writeup for Puppy class that includes characteristics and behaviors.

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+#SPECIES ------------------------
+#Puppy
+
+#CHARACTERISTICS ----------------
+#Fur color: varies
+#Friendly: varies
+#Breed: varies
+#Name: varies
+#Potty-trained: varies
+
+#BEHAVIOR -----------------------
+#Bark
+#Fetch
+#Sit
