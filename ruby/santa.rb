85893f9b13d2b221c9eb4ae884de196a9f9afd0f
Peter Haley <pblhaley@gmail.com>
2017-08-26 18:15:08 -0500

    Add attr_accessor for getter and setter methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,7 @@
 class Santa
 
+  attr_accessor :age, :ethnicity, :gender
+
   def initialize(gender, ethnicity)
     puts "Initializing Santa instance..."
     @gender = gender
@@ -16,6 +18,15 @@ class Santa
     puts "That was a good #{cookie_type} cookie!"
   end
 
+  def celebrate_birthday
+    @age += 1
+  end
+
+  def get_mad_at(reindeer_name)
+    bad_reindeer = @reindeer_ranking.delete(reindeer_name)
+    @reindeer_ranking.push(bad_reindeer)
+  end
+
 end
 
 ########## TEST CODE ##########
@@ -23,6 +34,11 @@ end
 #santa = Santa.new
 #santa.speak
 #santa.eat_milk_and_cookies("chocolate chip")
+#santa.celebrate_birthday
+#santa.get_mad_at("Vixen")
+#santa.age
+#santa.ethnicity
+#santa.gender = "prefer not to say"
 
 santas = []
 santa_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "queer", "prefer not to say"]
@@ -32,8 +48,15 @@ santa_genders.length.times do |i|
   santas << Santa.new(santa_genders[i], santa_ethnicities[i])
 end
 
-santas.each do
-  new_santa = Santa.new(santa_genders.sample, santa_ethnicities.sample)
-  p new_santa
+santas.each do |i|
+  i = Santa.new(santa_genders.sample, santa_ethnicities.sample)
+  i.celebrate_birthday
+  i.get_mad_at("Vixen")
+  i.age
+  i.ethnicity
+  i.gender = "prefer not to say"
+  p i
 end
+
+
 
