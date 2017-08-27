d4c6a662b7defd5934b110b25bbe53174bc5bd68
Peter Haley <pblhaley@gmail.com>
2017-08-27 16:00:14 -0500

    Add mixin should module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,40 @@
+#module Shout
+#  def self.yell_angrily(words)
+#    words + "!!!" + " :("
+#  end
+
+#  def self.yelling_happily(words)
+#    words + "!!!" + " :)"
+#  end
+#end
+
+#p Shout.yell_angrily("I'm mad")
+#p Shout.yelling_happily("I'm happy")
+
+#------------------------------------------
+
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
+  def yelling_happily(words)
+    words + "!!!" + " :)"
   end
 
-  def self.yelling_happily(words)
-    words + "!!!" + " :)"
+  def yell_angrily(words)
+    words + "!!!" + " :("
   end
 end
 
-p Shout.yell_angrily("I'm mad")
-p Shout.yelling_happily("I'm happy")
+class Coach
+  include Shout
+end
+
+class Cheerleader
+  include Shout
+end
+
+coach = Coach.new
+p coach.yelling_happily("I'm happy")
+p coach.yell_angrily("I'm angry")
+
+cheerleader = Cheerleader.new
+p cheerleader.yelling_happily("I'm happy")
+p cheerleader.yell_angrily("I'm angry")
