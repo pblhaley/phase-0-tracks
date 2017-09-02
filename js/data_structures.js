d9ada6bd4794a26a1ff37717e9ab66c3a218e839
Peter Haley <pblhaley@gmail.com>
2017-09-02 13:00:20 -0500

    Build a car object with a variety of variables and functions. Create driver code to test two separate cars

--- js/data_structures.js
+++ js/data_structures.js
@@ -15,3 +15,27 @@ for (var i = 0; i < colors.length; i++) {
 }
 
 console.log(horseObject);
+
+//------------------------------------------------------------
+
+function Car(model, year, isRegistered) {
+  this.model = model;
+  this.year = year;
+  this.isRegistered = isRegistered;
+  this.revsEngine = function() {console.log("Vroooom!");
+  };
+
+  console.log("CAR BUILD COMPLETE");
+}
+
+console.log("Let's build a car...");
+var newCar = new Car("Toyota", 2012, true);
+console.log(newCar);
+console.log("Let's fire it up!");
+newCar.revsEngine();
+
+console.log("Let's build second car...");
+var secondCar = new Car("Mazda", 2015, false);
+console.log(secondCar);
+console.log("Let's fire it up!");
+secondCar.revsEngine();
