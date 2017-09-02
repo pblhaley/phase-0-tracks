ad6f2fcca6fbfe29ddbb4b0f96e80ac6a029f67c
Peter Haley <pblhaley@gmail.com>
2017-09-02 12:34:31 -0500

    Write a function that pairs a name with a color and store it in a new object

--- js/data_structures.js
+++ js/data_structures.js
@@ -7,3 +7,11 @@ names.push("Jasper");
 
 console.log(colors);
 console.log(names);
+
+horseObject = {};
+
+for (var i = 0; i < colors.length; i++) {
+  horseObject[names[i]] = colors[i];
+}
+
+console.log(horseObject);
