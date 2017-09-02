119f25ccb037287445b957389ac5e84ad50ff196
Peter Haley <pblhaley@gmail.com>
2017-09-02 12:21:35 -0500

    Declare arrays for names and colors. Add names and colors to arrays

---
+++ js/data_structures.js
@@ -0,0 +1,9 @@
+var colors = ["sparkles", "glitter", "green", "bronze"];
+
+var names = ["Ed", "Marshall", "Princess Fancypants", "Whisper in the Wind"];
+
+colors.push("Rose");
+names.push("Jasper");
+
+console.log(colors);
+console.log(names);
