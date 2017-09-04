2569f7b393145ecc5c493e710ce5b9c54da4fa39
Peter Haley <pblhaley@gmail.com>
2017-09-04 11:13:34 -0500

    Refactor and add comment on my return true or false issues

--- js/algos.js
+++ js/algos.js
@@ -28,21 +28,25 @@ console.log(longestWord(["long word", "longer word", "longest word"]))
 //--------------- RELEASE 1 ---------------
 // Write a function that takes two objects and checks to see if the objects share at least one key-value pair.
 // Input: two objects with key-value pairs
-// Steps:
+// Steps: loop through both object keys
+//        if no matching keys then return false
+//        if matching keys then return true
 // Output: True or false depending on whether or not our two given objects contain a matching key-value pair
+
+// I think I've got this code right, or at least pretty close, but I can't get my function to return true or false without having to console.log the return. What gives?
 
 function matchingData (object1, object2) {
   for (var key1 in object1) {
     for (var key2 in object2) {
-      if (key1 == key2) {
-        if (object1[key1] == object2[key2]) {
-          return true;
-        }
-      }
-    }
-  }
-  return false;
+      if (key1 == key2 && object1[key1] == object2[key2]) {
+          console.log(true);
+      };
+    };
+  };
+  console.log(false);
 }
 
 matchingData({animal: "dog", legs: 4}, {animal: "pig", legs: 4})
 matchingData({animal: "dog", legs: 3}, {animal: "pig", legs: 4})
+
+
