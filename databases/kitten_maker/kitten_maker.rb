9ed799181bae70c31280db4a0a94ffb5e9b18863
Peter Haley <pblhaley@gmail.com>
2017-09-11 11:01:34 -0500

    Mess around with making different numbers of kittens

--- databases/kitten_maker/kitten_maker.rb
+++ databases/kitten_maker/kitten_maker.rb
@@ -26,13 +26,13 @@ db.execute(create_table_cmd)
 # db.execute("INSERT INTO kittens (name, age) VALUES ('Bob', 10)")
 
 # add LOOOOTS of kittens!
-# so. many. kittens. 
+# so. many. kittens.
 #KittenExplosion
 def create_kitten(db, name, age)
   db.execute("INSERT INTO kittens (name, age) VALUES (?, ?)", [name, age])
 end
 
-10000.times do
+1841.times do
   create_kitten(db, Faker::Name.name, 0)
 end
 
