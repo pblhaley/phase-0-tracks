21f180451b5bed61a1c26ac1c2a8530f331e385c
Peter Haley <pblhaley@gmail.com>
2017-09-12 01:31:07 -0500

    Tidy up pseudocode spacing

--- databases/restaurants/restaurant_suggester.rb
+++ databases/restaurants/restaurant_suggester.rb
@@ -26,22 +26,28 @@ def add_restaurant(db, name, type, wait_time, phone)
   db.execute("INSERT INTO restaurants (name, type, wait_time, phone) VALUES (?, ?, ?, ?)", [name, type, wait_time, phone])
 end
 
+# add restaurants
 add_restaurant(db, "Taco Joint", "Mexican", 45, "123-456-7890")
 add_restaurant(db, "Posada Margherita", "Italian", 30, "738-248-8642")
 add_restaurant(db, "Coat & Thai", "Thai", 15, "244-677-2497")
 add_restaurant(db, "Nasha", "Indian", 60, "743-431-2362")
 
 
-puts "Looks like somebody is hungry, but it's already past midnight! Let's see what's still open...got it! The only places open right now are Italian, Mexican, Thai, or Indian. Which do you choose?"
+#---------------DRIVER CODE---------------
+
+# get user input on type of food
+puts "Looks like somebody is hungry, but it's already past midnight! Let's see what's still open...got it! The only places open right now are Italian, Mexican, Thai, or Indian. Which are you craving?"
 
 user_choice = gets.chomp
 
+# select choice pieces of data based on user input
 restaurant_info = db.execute("SELECT restaurants.name FROM restaurants WHERE type=user_choice")
 
 wait_info = db.execute("SELECT restaurants.wait_time FROM restaurants WHERE type=user_choice")
 
 phone_info = db.execute("SELECT restaurants.phone FROM restaurants WHERE type=user_choice")
 
+# return info about user input
 puts "How about #{restaurant_info}? The wait is about #{wait_info} minutes."
 
 puts "Would you like to order? Please answer 'yes' or 'no'."
